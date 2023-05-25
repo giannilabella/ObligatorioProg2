c2faf71d0525c8434e34f424b77efc78c62272a3
Gianni Labella <gianni0labella@gmail.com>
2023-05-25 13:50:23 -0300

    Implement LinkedStack with Tests (JUnit)

--- src/adts/stack/MyStack.java
+++ src/adts/stack/MyStack.java
@@ -15,7 +15,7 @@ public interface MyStack<ElementT> extends MyCollection<ElementT> {
 
     ElementT peek();
 
-    boolean equals(Object obj);
+    boolean equals(Object other);
 
     boolean isEmpty();
 
