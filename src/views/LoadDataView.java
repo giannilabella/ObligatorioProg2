ef17cda5d93180a050eb0254c16802158e645d16
Gianni Labella <gianni0labella@gmail.com>
2023-06-24 18:49:36 -0300

    Update Menu Views
    Co-authored-by: Guzmán Vilche <131908084+GuzmanVil@users.noreply.github.com>

---
+++ src/views/LoadDataView.java
@@ -0,0 +1,69 @@
+package views;
+
+import controllers.DataLoaderController;
+import controllers.DriverController;
+import controllers.TweetController;
+import controllers.UserController;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class LoadDataView {
+    public static void loadData() {
+        String driversFilename = "drivers.txt";
+        String completeDatasetFilename = "f1_dataset.csv";
+        String reducedDatasetFilename = "f1_dataset_test.csv";
+
+        System.out.println("========= Choose the dataset file to load =========");
+        System.out.println("---------------------------------------------------");
+        System.out.println("1 - Load complete dataset (" + completeDatasetFilename + ")");
+        System.out.println("2 - Load reduced test dataset (" + reducedDatasetFilename + ")");
+        System.out.println("---------------------------------------------------");
+        System.out.print("Enter the selected option: ");
+
+        Scanner scanner = new Scanner(System.in);
+        final int selection = scanner.nextInt();
+
+        String datasetFilename;
+        if (selection == 1) {
+            System.out.println("============ Loading complete dataset! ============\n");
+            datasetFilename = completeDatasetFilename;
+        } else {
+            System.out.println("========== Loading reduced test dataset! ==========\n");
+            datasetFilename = reducedDatasetFilename;
+        }
+
+        loadDrivers(driversFilename);
+        loadDataset(datasetFilename);
+    }
+
+    private static void loadDrivers(String filename) {
+        System.out.println("Loading drivers from " + filename);
+        System.out.println("Reading file...");
+        final long startTime = System.nanoTime();
+
+        DataLoaderController.loadDrivers(Path.of(filename));
+        final long elapsedTime = System.nanoTime() - startTime;
+        System.out.println("Reading drivers file took " + TimeUnit.MILLISECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS) + " milliseconds!");
+
+        DriverController driverController = DriverController.getInstance();
+        System.out.println("Loaded " + driverController.getDriversCount() + " drivers!\n");
+    }
+
+    private static void loadDataset(String filename) {
+        System.out.println("Loading dataset from " + filename);
+        System.out.println("Reading file...");
+        final long startTime = System.nanoTime();
+
+        DataLoaderController.loadDataset(Path.of(filename));
+        final long elapsedTime = System.nanoTime() - startTime;
+        System.out.println("Reading dataset file took " + TimeUnit.MILLISECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS) + " milliseconds!");
+
+        UserController userController = UserController.getInstance();
+        System.out.println("Loaded " + userController.getUsersCount() + " users!");
+
+        TweetController tweetController = TweetController.getInstance();
+        System.out.println("Loaded " + tweetController.getTweetCount() + " tweets!\n");
+    }
+}
---
+++ src/views/MainMenuView.java
@@ -0,0 +1,109 @@
+package views;
+
+import controllers.DriverController;
+import entities.MentionedDriver;
+
+import java.util.Scanner;
+
+public class MainMenuView {
+    public static void main(String[] args) {
+        LoadDataView.loadData();
+
+        int selection = -1;
+        while (selection != 0) {
+            System.out.println("====== Choose the one of the following reports ======");
+            System.out.println("-----------------------------------------------------");
+            System.out.println("1 - Top 10 most mentioned drivers in a month and year");
+            System.out.println("2 - Top 15 users with more tweets");
+            System.out.println("3 - Number of different hashtags for a given day");
+            System.out.println("4 - Most used hashtag for a given day (excluding #F1)");
+            System.out.println("5 - Top 7 users with most favorites");
+            System.out.println("6 - Number of tweets with a specific word or phrase");
+            System.out.println("0 - Exit");
+            System.out.println("-----------------------------------------------------");
+            System.out.print("Enter the selected option: ");
+
+            Scanner scanner = new Scanner(System.in);
+            selection = scanner.nextInt();
+            switch (selection) {
+                case 1:
+                    System.out.println("========== Listing most mentioned drivers! ==========\n");
+                    getMostMentionedDrivers(10);
+                    break;
+                case 2:
+                    System.out.println("========== Listing users with more tweets! ==========\n");
+                    getMostActiveUsers(15);
+                    break;
+                case 3:
+                    System.out.println("======= Getting number of different hashtags! =======\n");
+                    getNumberOfDifferentHashtags();
+                    break;
+                case 4:
+                    System.out.println("======= Getting most used hashtag in the day! =======\n");
+                    getMostUsedHashtag();
+                    break;
+                case 5:
+                    System.out.println("=========== Listing most favorited users! ===========\n");
+                    getMostFavoritedUsers(7);
+                    break;
+                case 6:
+                    System.out.println("============= Getting number of tweets! =============\n");
+                    getNumberOfTweetWithWordOrPhrase();
+                    break;
+                case 0:
+                    System.out.println("====================== Exiting! =====================\n");
+                    break;
+                default:
+                    System.out.println("================== Invalid option! ==================\n");
+                    break;
+            }
+        } 
+    }
+
+    private static void getMostMentionedDrivers(int numberOfDrivers) {
+        Scanner scanner = new Scanner(System.in);
+
+        DriverController driverController = DriverController.getInstance();
+        for (MentionedDriver driver: driverController.getMostMentionedDriversByMonthAndYear((byte) 8, (short) 2021, numberOfDrivers)) {
+            System.out.println("Driver " + driver.getFullName() + " is mentioned " + driver.mentionsCount() + " times");
+        }
+
+        System.out.println("\nPress enter to go back to the menu");
+        scanner.nextLine();
+    }
+    
+    private static void getMostActiveUsers(int numberOfUsers) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nPress enter to go back to the menu");
+        scanner.nextLine();
+    }
+    
+    private static void getNumberOfDifferentHashtags() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nPress enter to go back to the menu");
+        scanner.nextLine();
+    }
+    
+    private static void getMostUsedHashtag() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nPress enter to go back to the menu");
+        scanner.nextLine();
+    }
+    
+    private static void getMostFavoritedUsers(int numberOfUsers) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nPress enter to go back to the menu");
+        scanner.nextLine();
+    }
+
+    private static void getNumberOfTweetWithWordOrPhrase() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nPress enter to go back to the menu");
+        scanner.nextLine();
+    }
+}
