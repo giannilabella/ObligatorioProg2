d3047f49fb0b74a8f5f508d6173281601fc8420c
Guzman <gvilche@corre.um.edu.uy>
2023-06-25 15:50:23 -0300

    Mentioned Drivers

--- src/views/MainMenuView.java
+++ src/views/MainMenuView.java
@@ -1,23 +1,17 @@
 package views;
 
 import controllers.DriverController;
-import controllers.HashtagController;
 import controllers.UserController;
 import entities.MentionedDriver;
-import entities.UsedHashtag;
 import entities.User;
 import uy.edu.um.prog2.adt.collection.MyCollection;
 
-import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class MainMenuView {
     public static void main(String[] args) {
-        LoadDataView.loadData();
+        //LoadDataView.loadData();
 
-        Scanner scanner = new Scanner(System.in);
         int selection = -1;
         while (selection != 0) {
             System.out.println("====== Choose the one of the following reports ======");
@@ -32,163 +26,108 @@ public class MainMenuView {
             System.out.println("-----------------------------------------------------");
             System.out.print("Enter the selected option: ");
 
-
-            try {
-                selection = scanner.nextInt();
-            } catch (NoSuchElementException ignored) {
-                selection = -1;
-            }
+            Scanner scanner = new Scanner(System.in);
+            selection = scanner.nextInt();
             switch (selection) {
-                case 1 -> {
+                case 1:
                     System.out.println("========== Listing most mentioned drivers! ==========\n");
-                    getMostMentionedDrivers();
-                }
-                case 2 -> {
+                    getMostMentionedDrivers(10);
+                    break;
+                case 2:
                     System.out.println("========== Listing users with more tweets! ==========\n");
-                    getMostActiveUsers();
-                }
-                case 3 -> {
+                    getMostActiveUsers(15);
+                    break;
+                case 3:
                     System.out.println("======= Getting number of different hashtags! =======\n");
                     getNumberOfDifferentHashtags();
-                }
-                case 4 -> {
+                    break;
+                case 4:
                     System.out.println("======= Getting most used hashtag in the day! =======\n");
                     getMostUsedHashtag();
-                }
-                case 5 -> {
+                    break;
+                case 5:
                     System.out.println("=========== Listing most favorited users! ===========\n");
-                    getMostFavoritedUsers();
-                }
-                case 6 -> {
+                    getMostFavoritedUsers(7);
+                    break;
+                case 6:
                     System.out.println("============= Getting number of tweets! =============\n");
                     getNumberOfTweetWithWordOrPhrase();
-                }
-                case 0 -> System.out.println("====================== Exiting! =====================\n");
-                default -> System.out.println("================== Invalid option! ==================\n");
+                    break;
+                case 0:
+                    System.out.println("====================== Exiting! =====================\n");
+                    break;
+                default:
+                    System.out.println("================== Invalid option! ==================\n");
+                    break;
             }
         } 
     }
 
-    private static void getMostMentionedDrivers() {
+    private static void getMostMentionedDrivers(int numberOfDrivers) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Insert the month: ");
         byte month;
-        try {
-            month = scanner.nextByte();
-        } catch (InputMismatchException ignored) {
-            System.out.println("Invalid month!");
-            waitForEnter();
-            return;
-        }
-
-        System.out.print("Insert the year: ");
         short year;
-        try {
-            year = scanner.nextShort();
-        } catch (InputMismatchException ignored) {
-            System.out.println("Invalid year!");
-            waitForEnter();
+        month = scanner.nextByte();
+        System.out.println("Enter the month (1-12): ");
+        year = scanner.nextShort();
+        System.out.println("Enter the year (yyyy): ");
+
+        if(year == 2021 && month < 7 || year == 2022 && month > 8 ) {
+            System.out.println("Invalid date! The date must be after June 2021 and before September 2022");
+            System.out.println("\nPress enter to go back to the menu");
+            scanner.nextLine();
             return;
+        }else {
+            DriverController driverController = DriverController.getInstance();
+            MyCollection<MentionedDriver> mostMentionedDrivers = driverController.getMostMentionedDriversByMonthAndYear(month, year, numberOfDrivers);
+            for (MentionedDriver driver : mostMentionedDrivers) {
+                System.out.println("Driver " + driver.getFullName() + " is mentioned " + driver.mentionsCount() + " times");
+            }
         }
 
-        System.out.println();
-
-        DriverController driverController = DriverController.getInstance();
-        MyCollection<MentionedDriver> mostMentionedDrivers = driverController.getMostMentionedDriversByMonthAndYear(month, year, 10);
-        for (MentionedDriver driver: mostMentionedDrivers) {
-            System.out.println("Driver " + driver.getFullName() + " is mentioned " + driver.mentionsCount() + " times");
-        }
-
-        waitForEnter();
+        System.out.println("\nPress enter to go back to the menu");
+        scanner.nextLine();
     }
     
-    private static void getMostActiveUsers() {
+    private static void getMostActiveUsers(int numberOfUsers) {
+        Scanner scanner = new Scanner(System.in);
+
         UserController userController = UserController.getInstance();
-        MyCollection<User> mostActiveUsers = userController.getMostActiveUsers(15);
+        MyCollection<User> mostActiveUsers = userController.getMostActiveUsers(numberOfUsers);
         for (User user: mostActiveUsers) {
             System.out.println("User \"" + user.getName() + "\" has tweeted " + user.getTweetsCount() + " times and " + (user.isVerified() ? "is verified" : "is not verified"));
         }
 
-        waitForEnter();
+        System.out.println("\nPress enter to go back to the menu");
+        scanner.nextLine();
     }
     
     private static void getNumberOfDifferentHashtags() {
-        System.out.print("Insert the date (yyyy-mm-dd): ");
-        String date;
-        try {
-            Scanner scanner = new Scanner(System.in);
-            date = scanner.next("[0-9]{4}-[0-9]{2}-[0-9]{2}");
-        } catch (InputMismatchException ignored) {
-            System.out.println("Invalid date format!");
-            waitForEnter();
-            return;
-        }
+        Scanner scanner = new Scanner(System.in);
 
-        byte day = Byte.parseByte(date.substring(8, 10));
-        byte month = Byte.parseByte(date.substring(5, 7));
-        short year = Short.parseShort(date.substring(0, 4));
-
-        System.out.println();
-
-        HashtagController hashtagController = HashtagController.getInstance();
-        int numberOfDifferentHashtags = hashtagController.getNumberOfDifferentHashtagsInADay(day, month, year);
-        System.out.println("In the day " + date + ", " + numberOfDifferentHashtags + " different hashtags were used");
-
-        waitForEnter();
+        System.out.println("\nPress enter to go back to the menu");
+        scanner.nextLine();
     }
     
     private static void getMostUsedHashtag() {
-        System.out.print("Insert the date (yyyy-mm-dd): ");
-        String date;
-        try {
-            Scanner scanner = new Scanner(System.in);
-            date = scanner.next("[0-9]{4}-[0-9]{2}-[0-9]{2}");
-        } catch (InputMismatchException ignored) {
-            System.out.println("Invalid date format!");
-            waitForEnter();
-            return;
-        }
+        Scanner scanner = new Scanner(System.in);
 
-        byte day = Byte.parseByte(date.substring(8, 10));
-        byte month = Byte.parseByte(date.substring(5, 7));
-        short year = Short.parseShort(date.substring(0, 4));
-
-        System.out.println();
-
-        HashtagController hashtagController = HashtagController.getInstance();
-        UsedHashtag mostUsedHashtag = hashtagController.getMostUsedHashtagInTheDay(day, month, year);
-        if (mostUsedHashtag != null) {
-            System.out.println("In the day " + date + ", the most used hashtag is #" + mostUsedHashtag.getHashtagText() + " with " + mostUsedHashtag.getUsesCount() + " uses");
-        } else {
-            System.out.println("In the day " + date + ", no hashtags were used");
-        }
-
-        waitForEnter();
+        System.out.println("\nPress enter to go back to the menu");
+        scanner.nextLine();
     }
     
-    private static void getMostFavoritedUsers() {
-        UserController userController = UserController.getInstance();
-        MyCollection<User> mostFavoritedUsers = userController.getMostFavoritedUsers(7);
-        for (User user: mostFavoritedUsers) {
-            System.out.println("User \"" + user.getName() + "\" has " + user.getFavoritesCount() + " favorites");
-        }
+    private static void getMostFavoritedUsers(int numberOfUsers) {
+        Scanner scanner = new Scanner(System.in);
 
-        waitForEnter();
+        System.out.println("\nPress enter to go back to the menu");
+        scanner.nextLine();
     }
 
     private static void getNumberOfTweetWithWordOrPhrase() {
         Scanner scanner = new Scanner(System.in);
 
-        // TODO: get word or phrase and get number of tweets with it
-
-        waitForEnter();
-    }
-
-    private static void waitForEnter() {
         System.out.println("\nPress enter to go back to the menu");
-        try {
-            int ignored = System.in.read();
-        } catch (IOException ignored) {}
+        scanner.nextLine();
     }
 }
